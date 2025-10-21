1dafc662198bf18ebd5e85df7315b75ad836bc2a
JoseR200 <joserodrigolopez@icloud.com>
2025-10-21 14:11:11 +0200

    Clean code

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://www.aurumtech.site/quopa/',
+    sockerUrl: 'https://www.aurumtech.site/',
+};
---
+++ src/environment/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:3002/',
+    sockerUrl: 'http://localhost:3002/',
+};
